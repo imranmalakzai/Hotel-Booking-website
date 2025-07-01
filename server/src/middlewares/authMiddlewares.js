ffc125e00c689653e95297c28cceb5280306ccdd
imranmalakzai <afghanimran92@gmail.com>
2025-07-01 18:24:32 +0430

    pull the requrest

--- server/src/middlewares/authMiddlewares.js
+++ server/src/middlewares/authMiddlewares.js
@@ -1,9 +1,10 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/apiError.js";
 import { User } from "../models/user.model.js";
+import { getAuth } from "@clerk/express";
 
 const protectedRoute = asyncHandler(async (req, _, next) => {
-  const { userId } = req.auth;
+  const { userId } = getAuth(req);
   if (!userId) {
     throw new ApiError("not authenticated", 401);
   } else {
